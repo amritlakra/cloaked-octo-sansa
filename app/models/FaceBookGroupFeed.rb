7b2d86b634de6bb761f4a48c86f3eb479cfe6491
Amrit <amritlakra@live.com>
2013-12-01 00:57:32 +0530

    test

--- app/models/FaceBookGroupFeed.rb
+++ app/models/FaceBookGroupFeed.rb
@@ -2,11 +2,11 @@ class FaceBookGroupFeed
   attr_accessor  :postedBy, :groupName, :postMessage, :postType, :postedAt
 
   def initialize(fbHash)
-  		@postedBy=fbHash[from][name]
-  		@groupName=fbHash[to][data].fetch(0,'could not find group name')[name]
-  		@postMessage=fbHash[message]
-  		@postType=fbHash[type]
-  		@postedAt=Time.at(fbHash[created_time])
+  		@postedBy=fbHash['from']['name']
+  		@groupName=fbHash['to']['data'].fetch(0,'could not find group name')['name']
+  		@postMessage=fbHash['message']
+  		@postType=fbHash['type']
+  		@postedAt=Time.at(fbHash['created_time'])
   end
   
 end
--- app/models/FaceBookLinkFeed.rb
+++ app/models/FaceBookLinkFeed.rb
@@ -2,9 +2,9 @@ class FaceBookLinkFeed < FaceBookGroupFeed
   attr_accessor  :feedLink,:feedCaption,:feedName,:feedDescription
   def initialize(fbHash)
   	super(fbHash)
-  	@feedLink=fbHash[link]
-  	@feedCaption=fbHash[caption]
-  	@feedName=fbHash[name]
-  	@feedDescription=fbHash[description]
+  	@feedLink=fbHash['link']
+  	@feedCaption=fbHash['caption']
+  	@feedName=fbHash['name']
+  	@feedDescription=fbHash['description']
   end	
 end
