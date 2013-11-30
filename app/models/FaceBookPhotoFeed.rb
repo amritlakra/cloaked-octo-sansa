2acb7a65cd44309eb2b0299a96786b609d1e851c
Amrit <amritlakra@live.com>
2013-12-01 00:29:17 +0530

    test

--- app/models/FaceBookPhotoFeed.rb
+++ app/models/FaceBookPhotoFeed.rb
@@ -1,14 +1,14 @@
 class FaceBookPhotoFeed < FaceBookGroupFeed
   attr_accessor  :photoThubmNail,:photoName,:photoLink,:photoCaption,:byName,:byText,:byLink
   def initialize(fbHash)
-  	@photoThubmnail=fbHash.picture
-  	@photoLink=fbHash.link 
-  	@photoName=fbHash.name 
-  	@photoCaption=fbHash.caption
-  	if(fbHash.properties.fetch(0).present?)
-  		@byName=fbHash.properties.fetch(0).name
-  		@byText=fbHash.properties.fetch(0).text 
-  		@byLink=fbHash.properties.fetch(0).href 
+  	@photoThubmnail=fbHash[picture]
+  	@photoLink=fbHash[link]
+  	@photoName=fbHash[name]
+  	@photoCaption=fbHash[caption]
+  	if(fbHash[properties].fetch(0).present?)
+  		@byName=fbHash[properties]fetch(0)[name]
+  		@byText=fbHash[properties]fetch(0)[text]
+  		@byLink=fbHash[properties]fetch(0)[href]
   	else
   		@byName=''	
   		@byLink=''
