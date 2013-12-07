d157ae98646038242d2b578e9ff7a55fd6824ed1
Amrit <amritlakra@live.com>
2013-12-07 23:44:24 +0530

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Socialfeeds::Application.routes.draw do
   root to: "sessions#new"
   get "/auth/:provider/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy", :as => "logout"
-  get "/groups/index", to: "groups#index",:as => "index_group"
-  get "/groups/new", to: "groups#new",:as =>"new_group"
+  resources :groups
 end
