96a8260507c477e0409ee8bc64eb1df0db272e37
Amrit <amritlakra@live.com>
2013-12-08 15:58:12 +0530

    test

--- app/models/FaceBookLinkFeed.rb
+++ app/models/FaceBookLinkFeed.rb
@@ -1,10 +1,15 @@
 class FaceBookLinkFeed < FaceBookGroupFeed
-  attr_accessor  :feedLink,:feedCaption,:feedName,:feedDescription
+  attr_accessor  :feedLink,:feedCaption,:feedName,:feedDescription,:feedPicture,:isPictureLink
   def initialize(fbHash,id)
   	super(fbHash,id)
   	@feedLink=fbHash['link']
   	@feedCaption=fbHash['caption']
   	@feedName=fbHash['name']
   	@feedDescription=fbHash['description']
+  	@isPictureLink=false
+  	if !fbHash['picture'].nil? and fbHash['picture'].present?
+	@isPictureLink=true  		
+  	@feedPicture=fbHash['picture']	
+  	end
   end	
 end
