754a6ca3f600187b343731968ba43810dfb2d06f
Amrit <amritlakra@live.com>
2013-12-10 10:59:18 +0530

    test

--- app/models/FaceBookPhotoFeed.rb
+++ app/models/FaceBookPhotoFeed.rb
@@ -4,18 +4,8 @@ class FaceBookPhotoFeed < FaceBookGroupFeed
 
   def initialize(fbHash,id,graph)
     super(fbHash,id)
-    hiResObject= graph.get_object(fbHash['object_id'])
-    if(!hiResObject.nil? and !hiResObject.empty?)
-      if(!hiResObject['source'].nil? and !hiResObject['source'].empty? and !hiResObject['source'].blank? )
-        @photoThumbNail=hiResObject['source']
-      else
-        @photoThumbNail=fbHash['picture']
-      end
-    else
-      @photoThumbNail=fbHash['picture']    
-    end  
-  	
-  	@photoLink=fbHash['link']
+    @photoThumbNail=fbHash['picture']    
+   	@photoLink=fbHash['link']
   	@photoName=auto_link(fbHash['name'],:html => { :target => '_blank' })
   	@photoCaption=auto_link(fbHash['caption'],:html => { :target => '_blank' })
   	if( !fbHash['properties'].nil? and !fbHash['properties'].fetch(0).nil?)
