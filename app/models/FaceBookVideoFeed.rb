5359c5f66f78b062f95839623b495475a8868cda
Amrit <amritlakra@live.com>
2014-01-15 21:44:45 +0530

    test

--- app/models/FaceBookVideoFeed.rb
+++ app/models/FaceBookVideoFeed.rb
@@ -2,17 +2,17 @@ class FaceBookVideoFeed < FaceBookGroupFeed
 	attr_accessor  :videoFeedLength, :videoFeedUrl, :videoFeedFaceBookUrl, :videoFeedName, :videoFeedDescription
    def initialize(fbHash,id,groupPictureUrl,graph)
    	super(fbHash,id,groupPictureUrl)
-   	feedPropeties=fbHash[properties]
+   	feedPropeties=fbHash['properties']
 
    	feedPropeties.each do |prop|
-   		if "Length"==prop[name]
-   			@videoFeedLength="Length: "+prop[text]
+   		if "Length"==prop['name']
+   			@videoFeedLength="Length: "+prop['text']
    		end	
    	end	
-   	@videoFeedUrl="https://www.facebook.com/video/embed?video_id="+fbHash[object_id]
-   	@videoFeedFaceBookUrl=fbHash[link]
-   	@videoFeedName=fbHash[name]
-   	@videoFeedDescription=fbHash[description]
+   	@videoFeedUrl="https://www.facebook.com/video/embed?video_id="+fbHash['object_id']
+   	@videoFeedFaceBookUrl=fbHash['link']
+   	@videoFeedName=fbHash['name']
+   	@videoFeedDescription=fbHash['description']
    end
 end
 
