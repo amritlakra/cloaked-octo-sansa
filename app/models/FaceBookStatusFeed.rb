519498f3b54e0117495eaa6cf6e817607af3f58f
Amrit <amritlakra@live.com>
2014-01-16 13:53:00 +0530

    test

--- app/models/FaceBookStatusFeed.rb
+++ app/models/FaceBookStatusFeed.rb
@@ -1,10 +1,12 @@
 class FaceBookStatusFeed < FaceBookGroupFeed
-	attr_accessor  :statusFeedUrl,
+	attr_accessor  :statusFeedUrl
    def initialize(fbHash,id,groupPictureUrl)
-   	super(fbHash,id,groupPictureUrl)
-   	statusAction=fbHash['actions']
+   		super(fbHash,id,groupPictureUrl)
+   		statusAction=fbHash['actions']
+
 	   	statusAction.each do |feedActions|
 	   		@statusFeedUrl=feedActions['link']
 	   	end	
+
    end 
 end
